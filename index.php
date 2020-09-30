6fd11543069103ec85323d14830411f7bf69e8f8
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-30 11:11:01 -0400

    Autocommit por GitHub: alterações: add 'index.php'

--- index.php
+++ index.php
@@ -40,17 +40,6 @@
     </style>
 </head>
 <body>
-    <div class="pagination">
-    <a href="#">&laquo;</a>
-    <a href="#">1</a>
-    <a class="active" href="#">2</a>
-    <a href="#">3</a>
-    <a href="#">4</a>
-    <a href="#">5</a>
-    <a href="#">6</a>
-    <a href="#">&raquo;</a>
-    </div>
-
     <?php 
         $data = file_get_contents('person.json'); // put the contents of the file into a variable
         $persons = json_decode($data); // decode the JSON feed
@@ -70,5 +59,16 @@
             echo $persons[$i]->name;
         }
     ?>
+
+<div class="pagination">
+    <a href="#">&laquo;</a>
+    <a href="#">1</a>
+    <a class="active" href="#">2</a>
+    <a href="#">3</a>
+    <a href="#">4</a>
+    <a href="#">5</a>
+    <a href="#">6</a>
+    <a href="#">&raquo;</a>
+    </div>
 </body>
 </html>
