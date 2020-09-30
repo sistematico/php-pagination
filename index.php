c74c5a39e231d0825ea635bdd3e1cede5f1984be
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-30 11:14:01 -0400

    Autocommit por GitHub: alterações: add 'index.php'

--- index.php
+++ index.php
@@ -59,8 +59,61 @@
             echo $persons[$i]->name;
         }
     ?>
+    
+    <div class="pagination">
 
-<div class="pagination">
+    <?php
+
+        if ($totalpage == 0) {
+            echo 'Page 0 of 0'; 
+        } else { 
+            $nav_page = '<div class="navpage"><span class="current">Page '.$page.' of '.$totalpage.': </span>'; 
+            $limit_nav = 3; 
+            $start = ($page - $limit_nav <= 0) ? 1 : $page - $limit_nav; 
+            $end = $page + $limit_nav > $totalpage ? $totalpage : $page + $limit_nav; 
+            if($page + $limit_nav >= $totalpage && $totalpage > $limit_nav * 2){ 
+                $start = $totalpage - $limit_nav * 2; 
+            } 
+
+            if($start != 1){ //show first page 
+                $nav_page .= '<span class="item"><a href="'.sprintf($link, 1).'"> [1] </a></span>'; 
+            } 
+
+            if($start > 2){ //add ... 
+                $nav_page .= '<span class="current">...</span>'; 
+            } 
+
+            if($page > 5){ //add prev 
+                $nav_page .= '<span class="item"><a href="'.sprintf($link, $page-5).'">&laquo;</a></span>'; 
+            } 
+
+            for($i = $start; $i <= $end; $i++) { 
+                if($page == $i) 
+                    $nav_page .= '<span class="current">'.$i.'</span>'; 
+                else 
+                    $nav_page .= '<span class="item"><a href="'.sprintf($link, $i).'"> ['.$i.'] </a></span>'; 
+            } 
+
+            if($page + 3 < $totalpage){ //add next 
+                $nav_page .= '<span class="item"><a href="'.sprintf($link, $page+4).'">&raquo;</a></span>'; 
+            } 
+
+            if ($end + 1 < $totalpage){ //add ... 
+                $nav_page .= '<span class="current">...</span>'; 
+            }  
+
+            if ($end != $totalpage) //show last page 
+                $nav_page .= '<span class="item"><a href="'.sprintf($link, $totalpage).'"> ['.$totalpage.'] </a></span>'; 
+            $nav_page .= '</div>'; 
+            echo $nav_page; 
+        } 
+
+    ?>
+
+
+
+
+
     <a href="#">&laquo;</a>
     <a href="#">1</a>
     <a class="active" href="#">2</a>
@@ -69,6 +122,6 @@
     <a href="#">5</a>
     <a href="#">6</a>
     <a href="#">&raquo;</a>
-    </div>
+</div>
 </body>
 </html>
